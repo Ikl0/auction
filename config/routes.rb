a34b45bab355366313743ea8cf61109548eb10e2
ikl0 <klimovs2008van@gmail.com>
2023-04-29 12:06:13 +0300

    add routes for lots

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :lots
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', confirmations: 'users/confirmations' }
   root 'main#home'
 end

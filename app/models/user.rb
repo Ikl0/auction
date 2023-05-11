19b44c7cb90a7fb48e24c2de0468b70bd57d45ab
ikl0 <klimovs2008van@gmail.com>
2023-05-11 16:53:26 +0300

    add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :confirmable,
          :validatable, :omniauthable, omniauth_providers: [:github]
 
+  has_many :lots
+
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first

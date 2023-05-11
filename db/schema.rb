318788870d464c490f29a291fb76d006374bd96c
ikl0 <klimovs2008van@gmail.com>
2023-05-11 16:54:02 +0300

    add index

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,17 @@
-ActiveRecord::Schema[7.0].define(version: 2023_04_29_083547) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_093119) do
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "lots", force: :cascade do |t|
@@ -10,6 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_29_083547) do
     t.datetime "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_lots_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|

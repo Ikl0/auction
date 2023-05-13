fa8c61cc85bf884623c2373b2e02e152ca8bb2a9
ikl0 <klimovs2008van@gmail.com>
2023-05-13 20:25:52 +0300

    add routes for sidekiq/web

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+require 'sidekiq/web'
+require 'sidekiq/cron/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   resources :lots
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', confirmations: 'users/confirmations' }
   root 'main#home'

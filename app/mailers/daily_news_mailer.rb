60498b54081db027883097312ed94b2f390296e0
ikl0 <klimovs2008van@gmail.com>
2023-05-15 16:11:22 +0300

    remove lots_amount

--- app/mailers/daily_news_mailer.rb
+++ app/mailers/daily_news_mailer.rb
@@ -1,7 +1,6 @@
 class DailyNewsMailer < ApplicationMailer
-  def daily_email(user, lots_amount)
+  def daily_email(user)
     @user = user
-    @lots_amount = lots_amount
     mail to: user.email, subject: 'Daily Morning Info'
   end
 end

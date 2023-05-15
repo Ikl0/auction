31be7de646cc0707bb0fd876833badc5d13ad4cd
ikl0 <klimovs2008van@gmail.com>
2023-05-15 19:33:29 +0300

    add methods to handle tags

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,3 +1,15 @@
 class Lot < ApplicationRecord
   belongs_to :user
+
+  validates :name, :description, presence: true
+
+  def tags=(value)
+    value = value.split(', ').map(&:strip) if value.is_a?(String)
+    super(value)
+  end
+
+  def self.find_by_tags(tags)
+    array_of_tags = PG::TextEncoder::Array.new.encode(tags)
+    where('tags ?| :tags', tags: array_of_tags)
+  end
 end

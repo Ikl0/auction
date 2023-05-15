179ed422c61615a645c385f3a32d2c7ab86e0305
ikl0 <klimovs2008van@gmail.com>
2023-05-15 16:11:17 +0300

    remove lots_amount

--- app/workers/daily_news_worker.rb
+++ app/workers/daily_news_worker.rb
@@ -2,10 +2,9 @@ class DailyNewsWorker
   include Sidekiq::Worker
 
   def perform
-    recipients = User.all_users
-    lots_amount = Lot.count_all_lots
+    recipients = User.all
     recipients.each do |recipient|
-      DailyNewsMailer.daily_email(recipient, lots_amount).deliver_now
+      DailyNewsMailer.daily_email(recipient).deliver_now
     end
   end
 end

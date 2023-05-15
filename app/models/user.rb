1814c61bcb7a29e26ecf81154c3ca810d75824af
ikl0 <klimovs2008van@gmail.com>
2023-05-15 16:04:43 +0300

    add dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :confirmable,
          :validatable, :omniauthable, omniauth_providers: [:github]
 
-  has_many :lots
+  has_many :lots, dependent: :destroy
 
   def self.from_omniauth(access_token)
     data = access_token.info

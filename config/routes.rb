8c0c81cadd30c44d07443be56c586a867672200f
ikl0 <klimovs2008van@gmail.com>
2023-05-19 15:28:52 +0300

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :lots do
     get "/tag/:tag", on: :collection, to: "lots#tag", as: :tag
   end
+  get 'my_lots', to: 'lots#my_lots'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', confirmations: 'users/confirmations' }
   root 'main#home'
 end

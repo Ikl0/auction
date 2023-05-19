981bbdf6172ff8bedc9cbf648d83c5d6d731a0f3
ikl0 <klimovs2008van@gmail.com>
2023-05-19 22:10:55 +0300

    add routes for best lots

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     get "/tag/:tag", on: :collection, to: "lots#tag", as: :tag
   end
   get 'my_lots', to: 'lots#my_lots'
+  get '/best_lots', to: 'lots#best_lots', as: 'best_lots'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', confirmations: 'users/confirmations' }
-  root 'main#home'
+  root 'lots#best_lots'
 end

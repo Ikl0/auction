86fa24b9f69410ed44a37e8ede23b54a5c7f8eff
ikl0 <klimovs2008van@gmail.com>
2023-05-24 13:30:45 +0300

    refactor rubocop

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -1,6 +1,6 @@
 class LotsController < ApplicationController
   before_action :set_lot, only: %i[show edit update destroy]
-  before_action :authenticate_user!, except: [:index, :best_lots]
+  before_action :authenticate_user!, except: %i[index best_lots]
 
   # GET /lots or /lots.json
   def index

b5392ce5281c3b8abe688dcc642f10f0eb282863
ikl0 <klimovs2008van@gmail.com>
2023-05-31 21:37:54 +0300

    call worker from bid controller

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,6 +1,7 @@
 class BidsController < ApplicationController
   def create
     @lot = Lot.find(params[:lot_id])
+    @last_leading_bid = @lot.bids.order(amount: :desc).first
     @bid = @lot.bids.build(bid_params)
     @bid.user = current_user
 
@@ -10,6 +11,7 @@ class BidsController < ApplicationController
         redirect_to won_lots_path, notice: "Congratulations, you have won #{@lot.name}!"
       else
         redirect_to lot_path(@lot), notice: 'Your bid has been placed.'
+        BidNotificationWorker.perform_async(@lot.id, @last_leading_bid.id, @bid.id) if @last_leading_bid
       end
     else
       redirect_to lot_path(@lot), alert: @bid.errors.full_messages.to_sentence
